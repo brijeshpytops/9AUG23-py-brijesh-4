7f1214f64cda259c05550308a04548666d07a691
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2023-08-14 08:27:40 +0530

    add operatores file

---
+++ operatores.py
@@ -0,0 +1,62 @@
+# Arithmetic operators [+,-,*,/,%,//,**]
+# print(1/7)
+# x = 2
+# p = 4
+# print(x ** p)
+
+
+# Assignment operators [+=,-=,*=,/=,%=,//=,**=,<<=,>>=]
+# x = 10
+
+# x = x + 20
+# x += 20
+# print(x)
+
+
+# Comparison operators [=,==,!=, <, >, <=, >=]
+# x = 10
+# y = 10
+# z = x == y
+# z = x != y
+# z = x < y
+# z = x > y
+# z = x >= y
+# z = x <= y
+# print(z)
+
+# Logical operators [and, or, not]
+
+# A B C
+# T T T T T
+# T F F T F
+# F T F T F
+# F F F F F
+
+# z = (10 > 20)
+# # print(not z) # False
+# print(not z) # True
+
+
+# Identity operators [ is, is not]
+# x = 10
+# y = 10
+# print(y is x)
+# print(y is not x)
+
+
+# Membership operators [ in, not in]
+# name = "python"
+# print('p' in name)
+# print('py' in name)
+# print('Py' in name)
+# print('pth' not in name)
+
+
+# Bitwise operators [&, |, ^]
+# AB&|^
+# 00000
+# 01011
+# 10011
+# 11110
+
+# <<, >>
